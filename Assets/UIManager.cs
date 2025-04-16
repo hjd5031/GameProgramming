9859ef48ab15ad2be043990b47cb891efa6f1891
hjd5031 <jdhwang0923@gmail.com>
2025-04-16 16:23:32 +0900

    Feat: Add hp bar(demo)

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,22 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class UIManager : MonoBehaviour
 {
-    public void OnButtonClick()
-    {
-        Debug.Log("Click Button");
-    }
+    public Button startButton;
+    public Button optionButton;
+    public Button shopButton;
+    private UnityAction _action;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _action = () => OnButtonClick(startButton.name);
+        startButton.onClick.AddListener(_action);
         
+        optionButton.onClick.AddListener(delegate { OnButtonClick(optionButton.name); });
+        
+        shopButton.onClick.AddListener(()=> OnButtonClick(shopButton.name) );
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    public void OnButtonClick(string msg)
+    {
+        Debug.Log($"Click Button: {msg}");
     }
 }
