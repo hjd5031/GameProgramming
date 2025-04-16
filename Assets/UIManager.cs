bc28400dd78227d37ab2d5a915aa4fef410a5e00
hjd5031 <jdhwang0923@gmail.com>
2025-04-16 12:00:45 +0900

    Feat:Add UI button

---
+++ Assets/UIManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        Debug.Log("Click Button");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
